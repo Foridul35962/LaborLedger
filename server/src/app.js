3ba8400e0271f3348c5da764f0b71e8b6c046530
Foridul35962 <foridulislam35962@gmail.com>
2026-02-07 02:27:08 +0600

    user model add

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
+// local import
+import errorHandler from './helpers/ErrorHandler.js'
+import adminRouter from './routes/admin.route.js'
+import authRouter from './routes/auth.route.js'
+
 const app = express()
 
 app.use(cors({
@@ -15,10 +20,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 //routes
-
+app.use('/api/auth', authRouter)
+app.use('/api/admin', adminRouter)
 
 app.get('/', (req, res) => {
     res.send('LaborLedge server is running ...')
 })
+
+//global error handler
+app.use(errorHandler)
 
 export default app
