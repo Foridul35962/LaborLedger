370889692b9d030b206e4165ff5cdb92a303f054
Shahadat-Hossen-008 <penaxavier078@gmail.com>
2026-02-11 17:45:37 +0600

    Feat: Create hero section carousel

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wp.dynamiclayers.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- client/src/component/heroSection/HeroSection.tsx
+++ client/src/component/heroSection/HeroSection.tsx
@@ -1,28 +1,42 @@
 "use client"
 import useEmblaCarousel from 'embla-carousel-react'
-
+import Fade from 'embla-carousel-fade'
+import {  NextButton, PrevButton, usePrevNextButtons } from '../ui/emblaCarouselButton/EmblaCarouselArrow'
+import heroData from '@/component/heroSection/heroData.json'
+import Image from 'next/image'
+import Autoplay from 'embla-carousel-autoplay'
 export function HeroSection() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Fade(),Autoplay({delay: 4000})])
 
-  const goToPrev = () => emblaApi?.scrollPrev()
-  const goToNext = () => emblaApi?.scrollNext()
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
 
   return (
-    <div className="position-relative">
+    <div className="relative">
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex touch-pan-y touch-pinch-zoom">
-          <div className="flex-[0_0_100%]">Slide 1</div>
-          <div className="flex-[0_0_100%]">Slide 2</div>
-          <div className="flex-[0_0_100%]">Slide 3</div>
+          {heroData.map((item) => (
+            <div key={item.id} className='flex-[0_0_100%] relative h-150'>
+              <Image src={item.imageUrl} alt={item.title} fill
+                     priority className='object-cover'/>
+              <div className='absolute top-1/2 left-0 flex flex-col  pl-15 w-1/2 text-white transform -translate-y-1/2'>
+                <span className='text-lg font-bold text-yellow-500'>{item.subTitle}</span>
+                <h2 className='text-3xl font-bold'>{item.title}</h2>
+                <p className='pt-2 text-sm'>{item.desc}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
-      <button className="mt-3" onClick={goToPrev}>
-        Scroll to prev
-      </button>
-      <button className="mt-3" onClick={goToNext}>
-        Scroll to next
-      </button>
+      <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} className= {`absolute top-[50%] left-0 transform -translate-y-1/2`}/>
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} className= {`absolute top-[50%] right-0 transform -translate-y-1/2`}/>
+        </div>
     </div>
   )
 }
